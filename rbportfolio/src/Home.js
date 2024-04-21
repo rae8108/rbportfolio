c56234f9ec4ae185c52a04626d858cf688c44612
rae8108 <146367231+rae8108@users.noreply.github.com>
2024-04-21 22:31:45 +0100

    created navbar components

---
+++ rbportfolio/src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "./Nav";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <div>
+        <h1> hi</h1>
+      </div>
+    </div>
+  );
+}
